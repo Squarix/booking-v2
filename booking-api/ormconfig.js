85aa0e0100fc3cf1fd4ffefd1d39e1fc58e40832
Uladzislau <uladzislau.bahdanouski@codex-soft.com>
2020-12-04 01:33:01 +0300

    Joined UI & API in 1 project && added docker-compose

---
+++ booking-api/ormconfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  username: 'admin',
+  password: 'admin',
+  database: 'booking-api',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
---
+++ run.sh
@@ -0,0 +1,2 @@
+ sudo docker-compose -f ./docker-compose.production.yml build --no-cache
+ sudo docker-compose -f ./docker-compose.production.yml up
